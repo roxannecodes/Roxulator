7744813beeb4b91030f614bcb1d4975f24595a7f
Roxanne <roxanne.shoar@gmail.com>
2021-06-24 12:36:12 +0000

    Change method and redo JS from scratch

--- script.js
+++ script.js
@@ -1,55 +1,16 @@
 
-// DOM calc variables
-let display = document.querySelector('.display');
-let buttons = document.querySelectorAll('button');
-let keys = document.querySelector('.calc-buttons');
+//DOM variables
+let display = document.querySelector("input");
+let operator = document.querySelectorAll(".operator");
+let number = document.querySelectorAll(".digit");
+let clear = document.querySelector(".clear");
 
+let decimal = document.querySelector(".decimal
+");
 
-//the calculator object (initial state for empty expression)
+let equals = document.querySelector(".equals");
 
-let calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
-
-//keep calculator screen updated
-function updateDisplay() {
-  display.value = calculator.displayValue;
-}
-updateDisplay();
-
-//add event listeners to all buttons via parent (container)
-keys.addEventListener('click', function (event) {
-
-  //access the clicked element (destructuring assignment-- {target}=event )
-  let target = event.target;
-
-  // Check if the clicked element is a button (If not, exit from the function)
-  if (!target.matches('button')) {
-    return;
-  }
-
-  if (target.classList.contains('operator')) {
-    console.log( "operator",target.value);
-   return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
-    return;
-  }
-
-  if (target.classList.contains('clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  console.log('digit', target.value);
-});
-
-//study break:figure out the hard stuff on paper and study mdn
+let buttons = document.querySelectorAll("button");
 
 //Main calculation function
 function operate (num1, operator, num2) {
