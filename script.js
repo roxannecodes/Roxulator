56b954c3dab3e6260d87fed57a6adff0f3a215b9
Roxanne <roxanne.shoar@gmail.com>
2021-06-26 17:03:54 +0000

    Added "clear" button

--- script.js
+++ script.js
@@ -2,21 +2,24 @@
 
 let display = document.querySelector(".input");
 let resultDisplayed=false;
+let waitingForSecondOperand=false;
 
-//add event listeners to numbers
+//add click handlers to all number buttons
 let numbers = document.querySelectorAll(".digit");
 
 for (i of numbers) {
   i.addEventListener("click", function(e) {
-
+    
     let currentSTring = this.innerHTML;
     if (resultDisplayed===false){
     display.innerHTML += this.innerHTML;}
+
+
   });
 }
  
 
-//adding event listeners to all operators
+//adding click handlers to all operator buttons
 let operators= document.querySelectorAll(".operator");
 
 for (i of operators) {
@@ -26,9 +29,7 @@ i.addEventListener("click", function (){
 }
 
 
-
-
-//adding event listener to equals btn and calling the main calculation function 
+//adding click handler to equals btn and calling the main calculation function 
 
 let result=document.querySelector (".equals");
 
@@ -73,10 +74,18 @@ function subtract(a, b) {
 } 
 
 
-//clearing the calc screen
-
+//clearing the calc screen 
 let clear = document.querySelector(".clear");
 
 clear.addEventListener("click", function() {
+  display.innerHTML= " ";
+});
+
+
+// resetting the calc (all clear)
+let allClear = document.querySelector(".all-clear");
+
+allClear.addEventListener("click", function() {
   location.reload();
 });
+
