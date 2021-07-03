2303862a2cb8da4e0bfe19a609443a4c8191b123
Roxanne <roxanne.shoar@gmail.com>
2021-07-03 17:17:47 +0000

    .

--- script.js
+++ script.js
@@ -1,59 +1,50 @@
 
 let display = document.querySelector(".input");
+let numbers = document.querySelectorAll(".digit");
+let operators= document.querySelectorAll(".operator");
+let number;
+let num1;
+let num2;
+let operator;
+let string;
+let ar = [string];
+let firstChar= ar[0];
+
+
+
+
 
 //add click handlers to all number buttons
-let numbers = document.querySelectorAll(".digit");
-
 for (i of numbers) {
   i.addEventListener("click", function() {
-    //update the display after each entry
     display.innerHTML += this.innerHTML; 
   });
 }
 
 //adding click handlers to all operator buttons
-let operators= document.querySelectorAll(".operator");
-
 for (i of operators) {
-i.addEventListener("click", function() {
-
-display.innerHTML = this.innerHTML;
-});
-}
-
-let string=display.innerHTML;
-let ar = [string];
-let firstChar= ar[0];
-
-let operator;
-let num1;
-let num2;
-
-//function that stores/displays values for variables above and passes them to main calc function as parameters.
-function prepCalc (){
-
-   if (firstChar==="+" || "-"|| "&divide;" || "&times;") {
+i.addEventListener ("click", function() {
+  
+if (firstChar==="+" || "-"|| "&divide;" || "&times;") {
+      //store current display value
+      number = display.innerHTML;
+      console.log (number);
        //get operator
-        operator = ar[0];
+        operator = this.innerHTML;
+        console.log (operator);
+
         //get num2
         num2 = parseFloat(string);
         //update display
         display.innerHTML=this.innerHTML;
    }
-      
-   else {
-      //store num1 
-      num1= parseFloat(string);
-      //update display with operator that was clicked
-      display.innerHTML = this.innerHTML;
- 
-   }
 
-  display.innerHtml= operate(num1, operator, num2);
+//else if first character of string is a number
+display.innerHTML = this.innerHTML;
 
+
+});
 }
-
-//prepCalc();
 
 
 // *** Main calculation function ***
@@ -90,18 +81,20 @@ function subtract(a, b) {
   display.innerHTML = a - b ;
 } 
 
+//equals button
+let equals = document.querySelector(".equals");
+equals.addEventListener("click", function (){
+  display.innerHTML= operate(num1,num2,operator)
+});
 
 //clearing the calc screen 
 let clear = document.querySelector(".clear");
-
 clear.addEventListener("click", function() {
   display.innerHTML= " ";
 });
 
-
 // resetting the calc (all clear)
 let allClear = document.querySelector(".all-clear");
-
 allClear.addEventListener("click", function() {
   location.reload();
 });
